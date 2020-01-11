b60fa064e139d400b88ae8c0c2f5a0529ebf3167
--global <marcobritoh@gmail.com>
2020-01-11 17:11:21 -0300

    create component home

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import Home from "./components/containers/Home";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return <Home/>;
   }
 }
 
